f08a309d3d5ddc3f97795868adb66376109a5b46
awdfaf <awdfaf@kakao.com>
2024-08-31 15:16:37 +0900

    스프링 컨테이너와 의존관계 주입

--- src/main/java/paymentservice/paymentservice/Client.java
+++ src/main/java/paymentservice/paymentservice/Client.java
@@ -1,11 +1,16 @@
 package paymentservice.paymentservice;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService(new SimpleExRateProvider());
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = beanFactory.getBean(PaymentService.class);
+
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
 
         System.out.println(payment);
