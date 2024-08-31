e7c8534660e96df2a06c8ea476913c3acb9401cd
awdfaf <awdfaf@kakao.com>
2024-08-31 15:22:28 +0900

    싱글톤 레지스트리

--- src/main/java/paymentservice/paymentservice/ObjectFactory.java
+++ src/main/java/paymentservice/paymentservice/ObjectFactory.java
@@ -1,16 +1,18 @@
 package paymentservice.paymentservice;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
-    }
-    @Bean
-    public ExRateProvider exRateProvider() {
-        return new SimpleExRateProvider();
-    }
+//    @Bean
+//    public PaymentService paymentService() {
+//        return new PaymentService(exRateProvider());
+//    }
+//    @Bean
+//    public ExRateProvider exRateProvider() {
+//        return new SimpleExRateProvider();
+//    }
 }
